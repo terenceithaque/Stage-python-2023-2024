ce4d188fdb97f2ec4ce40d63f5aa9646cae1e50c
Terence Le Thierry <terence.lethierry@protonmail.com>
2024-07-10 20:23:22 +0200

    Ajouté les fonctions deplacerGauche et deplacerBas

--- 2048/main.py
+++ 2048/main.py
@@ -166,31 +166,10 @@ def deplacer_nombres(grille, direction):
 
         # Logique du déplacement à mettre ci-dessous
 
-        for ligne in range(len(grille)): # Pour chaque ligne de la grille
-            #print(f"Longueur de la ligne n°{ligne} :", len(grille[ligne]))
-            for colonne in range(len(grille[ligne]) -1): # On parcoure les colonnes de la ligne
-                #print("Case actuelle :", grille[ligne][colonne], end=" ")
-                #print(f"Colonne dans la ligne n°{ligne} :", colonne)
-                for colonne2 in range(colonne+1, len(grille[ligne])): # Pour chaque colonne dans la ligne après la colonne actuelle
-                    case_suivante = grille[ligne][colonne2] # Nombre contenu dans la case après
-                    #print("Case suivante :", case_suivante, end= " ")
-                    if case_suivante > 0: # Si le nombre de la case suivante est supérieur à 0
-                        if grille[ligne][colonne] == case_suivante: # Si le nombre dans la case actuelle correspond à celui dans la case suivante
-                            grille[ligne][colonne] = grille[ligne][colonne2]*2 # On fusionne les nombres de la case actuelle et de la case suivante par une multiplication
-                            grille[ligne][colonne2] = 0 # On vide la case actuelle
-                            score.augmenter(grille[ligne][colonne]) # Augmenter le score du joueur
-                            meilleur_score.actualiser(score.valeur) # Actualiser le meilleur score
-                            meilleur_score.sauvegarder() # Sauvegarder le meilleur score
-
-                    else: # Si la case suivante est vide
-                        print(f"La case (coords. {ligne}, {colonne2}) est vide")
-                        if grille[ligne][colonne] > 0: # Mais que l'actuelle ne l'est pas
-                            grille[ligne][colonne2] = grille[ligne][colonne] # Déplacer le nombre de la colonne actuelle vers la colonne suivante
-                            grille[ligne][colonne] = 0 # Vider la case actuelle
-                        #grille[ligne][colonne] = generer_nombre_a_apparaitre() # On vide la case actuelle
+        
 
 
-
+        deplacerGauche(grille, direction=direction, score_var=score, max_score_var=meilleur_score) # Déplacer les nombres vers la gauche
         coords_apparation_nombre = coords_aleat(grille) # Générer des coordonnées aléatoires pour le nouveau nombre à générer
         print("Coordonnées du nombre à apparaitre :", coords_apparation_nombre)
         ligne_nombre = coords_apparation_nombre[0] # Ligne dans laquelle le nombre doit apparaître
@@ -206,67 +185,7 @@ def deplacer_nombres(grille, direction):
     if direction == "bas": # Si on doit déplacer les nombres vers le bas
         #print("Déplacement vers le bas")
 
-        # Logique du déplacement à mettre ci-dessous
-
-
-        for ligne in range(len(grille)): # On parcoure les lignes de la grille
-            #print("Numéro de la ligne :", ligne)
-            for colonne in range(len(grille[ligne])): # Pour chaque colonne de la ligne
-                nombre_case_actuelle = grille[ligne][colonne] # Nombre dans la case actuelle
-                for ligne2 in range(ligne-1, -1, -1): # Pour chaque ligne avant la ligne actuelle
-                    #print("Ligne précédent la ligne actuelle :", ligne2)
-                    if grille[ligne2][colonne] == nombre_case_actuelle and grille[ligne2][colonne] > 0: # Si le nombre contenu dans une des cases précédentes correspond au nombre de la case actuelle
-                        grille[ligne2][colonne] = 0 # On vide la case précédente
-                        grille[ligne][colonne] *= 2 # On fusionne les nombres des deux cases par une multiplication
-                        score.augmenter(grille[ligne][colonne]*2) # Augmenter le score du joueur
-                        meilleur_score.actualiser(score.valeur) # Actualiser le meilleur score
-                        meilleur_score.sauvegarder() # Sauvegarder le meilleur score
-                        break
-
-                    elif grille[ligne2][colonne] == 0 and nombre_case_actuelle > 0: # Si la case précédente est vide mais que l'actuelle ne l'est pas
-                        grille[ligne2][colonne] = nombre_case_actuelle # On déplace le nombre de la case actuelle sur la case précédente
-                        grille[ligne][colonne] = 0 # On vide la case actuelle
-
-
-
-
-                    
-
-                
-
-                        
-        for ligne in range(len(grille) -1): # Pour chaque ligne de la grille après la première
-            print("Numéro de la ligne :", ligne)
-            #print("Ligne actuelle :", grille[ligne])
-
-            if ligne == len(grille) -1:
-                print(f"La ligne n°{ligne} (compte à partir de 0) est la dernière de la grille !")
-                
-            for colonne in range(len(grille[ligne])): # Pour chaque colonne de la ligne
-                case_actuelle = grille[ligne][colonne] # Case actuelle
-                for ligne2 in range(ligne+1, len(grille)): # Pour chaque ligne après la ligne actuelle
-                    #print(ligne2)
-                    lignes_entre = grille[ligne+1: ligne2] # Lignes comprises entre la ligne actuelle et chaque ligne suivante
-                    print(f"Lignes entre {ligne} et {ligne2}: {lignes_entre}")
-                    case_suivante = grille[ligne2][colonne] # Case dans la ligne suivante
-
-                    
-
-                    if case_suivante == 0 and case_actuelle > 0: # Si la case de la ligne suivante est vide et que l'actuelle ne l'est pas
-                        grille[ligne2][colonne] = case_actuelle # Déplacer le nombre de la case actuelle vers la ligne en-dessous
-                        grille[ligne][colonne] = 0 # On vide la ligne actuelle
-                        break # On quitte la boucle immédiatement 
-
-                    else:
-                        break
-
-                                                 
-                
-                    
-        
-       
-                
-
+        deplacerBas(grille, direction=direction, score_var=score, max_score_var=meilleur_score) # Déplacer les nombres vers le bas
         coords_apparation_nombre = coords_aleat(grille) # Générer des coordonnées aléatoires pour le nouveau nombre à générer
         print("Coordonnées du nombre à apparaitre :", coords_apparation_nombre)
         ligne_nombre = coords_apparation_nombre[0] # Ligne dans laquelle le nombre doit apparaître
